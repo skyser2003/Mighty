b2fcaaece28b3c45e279bb414640ef539ce57770
Seung Ho Jang <shjang1992@gmail.com>
2015-12-02 01:51:39 +0900

    Unreal project initial commit

---
+++ Source/Mighty/MightyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MightyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIGHTY_API AMightyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
