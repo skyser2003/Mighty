cbf012a609cc08a1000f6c19faeabab7476dd8ca
Seung Ho Jang <shjang1992@gmail.com>
2015-12-20 03:27:47 +0900

    Card placemeny by user ID

--- Source/Mighty/MightyGameMode.cpp
+++ Source/Mighty/MightyGameMode.cpp
@@ -5,24 +5,88 @@
 
 #include "Card.h"
 
-AMightyGameMode::AMightyGameMode() : game(new Mighty::Game())
+AMightyGameMode::AMightyGameMode()
 {
 }
 
 void AMightyGameMode::InitGameState()
 {
-	int i = 0;
-	for (auto type : Mighty::CardTypes())
+	game.reset(new Mighty::Game());
+
+	game->Init(game);
+	game->StartNewRound();
+
+	for (const auto& pair : game->GetPlayers())
 	{
-		const auto location = FVector(0.0f, 0.0f, 0.f);
+		FVector2D direction;
 
-		// Spawn a block
-		auto* card = GetWorld()->SpawnActor<ACard>(location, FRotator(0, 0, 0));
-		std::string imgName = Mighty::Util::GetCardResourceName(type);
-		card->SetImage(imgName);
-		card->SetActorLocation(FVector(i * 50, -200, 100));
-		++i;
+		auto player = pair.second;
+		auto standard = FVector2D(0, 0);
 
-		cardImageList[type] = card;
+		switch (player->GetID())
+		{
+		case 0:
+		{
+			direction = FVector2D(1, 1);
+		}
+		break;
+
+		case 1:
+		{
+			direction = FVector2D(1, 1);
+		}
+		break;
+
+		case 2:
+		{
+			direction = FVector2D(-1, 1);
+		}
+		break;
+
+		case 3:
+		{
+			direction = FVector2D(-1, -1);
+		}
+		break;
+
+		case 4:
+		{
+			direction = FVector2D(1, -1);
+		}
+		break;
+		}
+
+		if (direction.X == -1.0f)
+		{
+			standard.X = 800;
+		}
+		if (direction.Y == -1.0f)
+		{
+			standard.Y = 600;
+		}
+
+		if (player->GetID() == 1)
+		{
+			standard.X += 800 / 3;
+		}
+
+		for (size_t i = 0; i < player->GetHandCardList().size(); ++i)
+		{
+			auto handCard = player->GetHandCardList()[i];
+			auto type = handCard->GetType();
+			float imageScale = 0.7f;
+
+			auto* card = GetWorld()->SpawnActor<ACard>(FVector(0, 0, 0), FRotator(0, 0, 0));
+			std::string imgName = Mighty::Util::GetCardResourceName(type);
+			card->SetImage(imgName);
+
+			auto position = FVector(standard.X + direction.X * (i + 0.5f) * imageScale * card->GetWidth(),
+				-200,
+				standard.Y + direction.Y * card->GetHeight() / 2 * imageScale);
+
+			card->SetActorLocation(position);
+
+			cardImageList[type] = card;
+		}
 	}
 }
